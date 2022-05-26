03449f6905e14f5cbe0f03ea01e45685ffea6f19
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-26 19:43:11 -0300

    Removido código desnecessario.

--- app/src/main/java/com/droidev/levantamento/Pastebin.java
+++ app/src/main/java/com/droidev/levantamento/Pastebin.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.text.InputType;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
